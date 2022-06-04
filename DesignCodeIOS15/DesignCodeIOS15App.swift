1f243ce041457c4bd510cb33384d105d5175bede
xueyong <1085523029@qq.com>
2022-06-04 16:09:36 +0800

    Initial Commit

---
+++ DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by 薛勇 on 2022/6/4.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
