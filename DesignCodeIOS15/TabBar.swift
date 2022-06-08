1440b7a12bb4a276ffffad3bd9186c8c6ce3b17f
xueyong <1085523029@qq.com>
2022-06-08 22:46:11 +0800

    feat: custom TabBar

---
+++ DesignCodeIOS15/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  DesignCodeIOS15
+//
+//  Created by 薛勇 on 2022/6/7.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
